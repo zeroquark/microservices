232a99d813225cd397027e26252d379c1a0afb81
Gonzalo Lagos <gonzalo.esteban.lagos@gmail.com>
2019-11-27 05:20:50 -0300

    Pushing image-service; an entity micro-service.
    It manages and represents an image.

---
+++ src/main/java/me/microservices/spring/entity/Image.java
@@ -0,0 +1,47 @@
+package me.microservices.spring.entity;
+
+public class Image {
+
+    private int id;
+    private String title;
+    private String url;
+
+    public Image(int id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
