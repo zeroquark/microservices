e998e735b2f8ab951e658298dd52c284c5d35aac
Gonzalo Lagos <gonzalo.esteban.lagos@gmail.com>
2019-11-27 04:58:44 -0300

    Upload Auth-Service based on JWT (JSON Web Token)

---
+++ src/main/java/me/microservices/authservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,99 @@
+package me.microservices.authservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // hard coding the users. All passwords must be encoded.
+        final List<AppUser> users = Arrays.asList(
+                new AppUser(1, "omar", encoder.encode("12345"), "USER"),
+                new AppUser(2, "admin", encoder.encode("12345"), "ADMIN")
+        );
+
+        for (AppUser appUser : users) {
+            if (appUser.getUsername().equals(username)) {
+                List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                    .commaSeparatedStringToAuthorityList("ROLE_" + appUser.getRole());
+
+                return new User(appUser.getUsername(), appUser.getPassword(), grantedAuthorities);
+            }
+        }
+
+        // If user not found. Throw this exception.
+        throw new UsernameNotFoundException("Username: " + username + " not found");
+    }
+
+    private static class AppUser {
+        private Integer id;
+        private String username, password;
+        private String role;
+
+        public AppUser(Integer id, String username, String password, String role) {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+
+        @Override
+        public String toString() {
+            return "AppUser{" +
+                    "id=" + id +
+                    ", username='" + username + '\'' +
+                    ", password='" + password + '\'' +
+                    ", role='" + role + '\'' +
+                    '}';
+        }
+    }
+
+}
